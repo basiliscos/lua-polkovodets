6eda14818df5960eaf288cfa15198a688c0de65d
Ivan Baidakou <basiliscos@openmailbox.org>
2015-12-20 03:41:29 +0300

    Do not show n/a classes in battleDetailsWindow

--- src/lua/polkovodets/gui/BattleDetailsWindow.lua
+++ src/lua/polkovodets/gui/BattleDetailsWindow.lua
@@ -32,35 +32,69 @@ function BattleDetailsWindow.create(engine)
   setmetatable(o, BattleDetailsWindow)
   o.ctx_bound = false
   o.drawing.content_fn = nil
+  o.content = {}
 
+  o.text = {
+    size  = 12,
+    font  = engine.renderer.theme:get_font('default', 12),
+  }
+
+  return o
+end
+
+function BattleDetailsWindow:_classy_battle_weapons(record)
+  local engine = self.engine
+
+  local sources = {
+    {"i", "participants"},
+    {"i", "casualities"},
+    {"p", "participants"},
+    {"p", "casualities"},
+  }
+  local data = {}
+  local available_classes = {}
+  for idx, source in pairs(sources) do
+    local side, property = source[1], source [2]
+    local property_data = record.results[side][property]
+    for wi_id, quantity in pairs(property_data) do
+      local wi = assert(engine.weapon_instance_for[wi_id])
+      local class = assert(wi:get_class())
+      local data_key = class.id .. "_" .. side .. "_" .. property
+      data[data_key] = (data[data_key] or 0) + quantity
+      available_classes[class.id] = true
+    end
+  end
+
+  -- print("data = " .. inspect(data))
+  local images = {}
+  local font = self.text.font
+  for key, quantity in pairs(data) do
+    local surface = font:renderUtf8(tostring(quantity), "solid", 0xFFFFFF)
+    images[key] = Image.create(engine.renderer.sdl_renderer, surface)
+  end
+
+  local classifyer = function(class_id, side, property)
+    local key = class_id .. "_" .. side .. "_" .. property
+    return images[key]
+  end
+  return classifyer, available_classes
+end
+
+function BattleDetailsWindow:_construct_gui(available_classes)
+  local engine = self.engine
   local weapon_classes = engine.unit_lib.weapons.classes.list
 
   assert (#weapon_classes > 0)
   -- print(inspect(weapon_classes))
   local class_icon = weapon_classes[1]:get_icon()
 
-  local text_size = 12
-  local font = engine.renderer.theme:get_font('default', text_size)
-  local text_color = 0xFFFFFF
-
   local create_image = function(surface)
     return Image.create(engine.renderer.sdl_renderer, surface)
   end
 
-  local was_label = create_image(font:renderUtf8(engine:translate('ui.window.battle_details.header.was'), "solid", text_color))
-  local casualities_label = create_image(font:renderUtf8(engine:translate('ui.window.battle_details.header.casualities'), "solid", text_color))
-
-  o.content = {
-    text = {
-      font  = font,
-      color = text_color,
-      size  = text_size,
-    },
-    header = {
-    },
-    mouse_click = nil,
-    mouse_move  = nil,
-  }
+  local font = self.text.font
+  local was_label = create_image(font:renderUtf8(engine:translate('ui.window.battle_details.header.was'), "solid", 0xFFFFFF))
+  local casualities_label = create_image(font:renderUtf8(engine:translate('ui.window.battle_details.header.casualities'), "solid", 0xFFFFFF))
 
   -- initiator columns
   local header_dx = 10
@@ -84,7 +118,7 @@ function BattleDetailsWindow.create(engine)
     dy    = labels[#labels].dy + labels[#labels].label.h + 1,
     w     = 1,
     h     = 0, -- to be defined later
-    color = text_color,
+    color = 0xFFFFFF,
   }
 
   -- passive columns
@@ -101,7 +135,8 @@ function BattleDetailsWindow.create(engine)
 
   local header_w = labels[#labels].dx + header_dx + labels[#labels].label.w + header_dx
   local header_h = labels[1].label.h + 2
-  o.content.header = {
+
+  local header = {
     w         = header_w,
     h         = header_h,
     labels    = labels,
@@ -110,7 +145,8 @@ function BattleDetailsWindow.create(engine)
 
   local lines = {}
   local line_dy = header_h + 2
-  for idx, class in ipairs(weapon_classes) do
+  local my_weapon_classes = _.select(weapon_classes, function(k, class) return available_classes[class.id] end)
+  for idx, class in ipairs(my_weapon_classes) do
     local icon = class:get_icon()
     local center_y = math.modf(line_dy + icon.h/2)
     local line = {
@@ -146,58 +182,25 @@ function BattleDetailsWindow.create(engine)
   end
   separator.h = line_dy - lines[1].dy
   -- print("lines = " .. inspect(lines))
-  o.content.lines = lines
 
   local content_w = header_w
   local content_h = line_dy
 
-  o.content.size = {
-    w = content_w,
-    h = content_h,
-  }
-  o.content.not_found = {
-    available = create_image(font:renderUtf8('-', "solid", 0xAAAAAA)),
-    hilight   = create_image(font:renderUtf8('-', "solid", 0xFFFFFF)),
+  local gui = {
+    header = header,
+    lines  = lines,
+    not_found = {
+      available = create_image(font:renderUtf8('-', "solid", 0xAAAAAA)),
+      hilight   = create_image(font:renderUtf8('-', "solid", 0xFFFFFF)),
+    },
+    content_size ={
+      w = content_w,
+      h = content_h,
+    }
   }
 
   -- print(string.format("content size %d x %d", content_w, content_h))
-  return o
-end
-
-function BattleDetailsWindow:_classy_battle_weapons(record)
-  local engine = self.engine
-
-  local sources = {
-    {"i", "participants"},
-    {"i", "casualities"},
-    {"p", "participants"},
-    {"p", "casualities"},
-  }
-  local data = {}
-  for idx, source in pairs(sources) do
-    local side, property = source[1], source [2]
-    local property_data = record.results[side][property]
-    for wi_id, quantity in pairs(property_data) do
-      local wi = assert(engine.weapon_instance_for[wi_id])
-      local class = assert(wi:get_class())
-      local data_key = class.id .. "_" .. side .. "_" .. property
-      data[data_key] = (data[data_key] or 0) + quantity
-    end
-  end
-
-  -- print("data = " .. inspect(data))
-  local images = {}
-  local font = self.content.text.font
-  for key, quantity in pairs(data) do
-    local surface = font:renderUtf8(tostring(quantity), "solid", 0xFFFFFF)
-    images[key] = Image.create(engine.renderer.sdl_renderer, surface)
-  end
-
-  local classifyer = function(class_id, side, property)
-    local key = class_id .. "_" .. side .. "_" .. property
-    return images[key] or self.content.not_found.available
-  end
-  return classifyer
+  return gui
 end
 
 function BattleDetailsWindow:bind_ctx(context)
@@ -205,11 +208,13 @@ function BattleDetailsWindow:bind_ctx(context)
   if (engine.state.popups.battle_details_window) then
     self.ctx_bound = true
     local record = assert(context.state.history_record)
+    local classifyer, available_classes = self:_classy_battle_weapons(record)
+    local gui = self:_construct_gui(available_classes)
 
     local theme = assert(context.renderer.theme)
     local details_ctx = _.clone(context, true)
-    local content_w = self.content.size.w
-    local content_h = self.content.size.h
+    local content_w = gui.content_size.w
+    local content_h = gui.content_size.h
     details_ctx.content_size = { w = content_w, h = content_h}
 
     local window = assert(context.window)
@@ -231,7 +236,6 @@ function BattleDetailsWindow:bind_ctx(context)
       return over
     end
 
-    local classifyer = self:_classy_battle_weapons(record)
 
     local sdl_renderer = assert(context.renderer.sdl_renderer)
     self.drawing.content_fn = function()
@@ -242,33 +246,33 @@ function BattleDetailsWindow:bind_ctx(context)
       ))
 
       -- unit classes icons with counts/casualities for initial/passive sides
-      for ignored, line in ipairs(self.content.lines) do
+      for ignored, line in ipairs(gui.lines) do
         local icon = line.icon
         assert(sdl_renderer:copy(icon.texture, nil,
           {x = content_x + line.dx, y = content_y + line.dy, w = icon.w, h = icon.h}
         ))
-        local i_was = classifyer(line.class_id, "i", "participants")
+        local i_was = classifyer(line.class_id, "i", "participants") or gui.not_found.available
         assert(sdl_renderer:copy(i_was.texture, nil,{
           x = math.modf(content_x + line.center.i.was.dx - i_was.w/2),
           y = math.modf(content_y + line.center.i.was.dy - i_was.h/2),
           w = i_was.w,
           h = i_was.h,
         }))
-        local i_casualities = classifyer(line.class_id, "i", "casualities")
+        local i_casualities = classifyer(line.class_id, "i", "casualities") or gui.not_found.available
         assert(sdl_renderer:copy(i_casualities.texture, nil,{
           x = math.modf(content_x + line.center.i.casualities.dx - i_casualities.w/2),
           y = math.modf(content_y + line.center.i.casualities.dy - i_casualities.h/2),
           w = i_casualities.w,
           h = i_casualities.h,
         }))
-        local p_was = classifyer(line.class_id, "p", "participants")
+        local p_was = classifyer(line.class_id, "p", "participants") or gui.not_found.available
         assert(sdl_renderer:copy(p_was.texture, nil,{
           x = math.modf(content_x + line.center.p.was.dx - p_was.w/2),
           y = math.modf(content_y + line.center.p.was.dy - p_was.h/2),
           w = p_was.w,
           h = p_was.h,
         }))
-        local p_casualities = classifyer(line.class_id, "p", "casualities")
+        local p_casualities = classifyer(line.class_id, "p", "casualities") or gui.not_found.available
         assert(sdl_renderer:copy(p_casualities.texture, nil,{
           x = math.modf(content_x + line.center.p.casualities.dx - p_casualities.w/2),
           y = math.modf(content_y + line.center.p.casualities.dy - p_casualities.h/2),
@@ -278,7 +282,7 @@ function BattleDetailsWindow:bind_ctx(context)
       end
 
       -- table header
-      for idx, label_info in ipairs(self.content.header.labels) do
+      for idx, label_info in ipairs(gui.header.labels) do
         local label = label_info.label
         assert(sdl_renderer:copy(label.texture, nil,
           {x = content_x + label_info.dx, y = content_y + label_info.dy, w = label.w, h = label.h}
@@ -287,15 +291,14 @@ function BattleDetailsWindow:bind_ctx(context)
 
       -- initiator/passive separator
       local prev_color = sdl_renderer:getDrawColor()
-      assert(sdl_renderer:setDrawColor(self.content.header.separator.color))
+      assert(sdl_renderer:setDrawColor(gui.header.separator.color))
       assert(sdl_renderer:drawLine({
-        x1 = content_x + self.content.header.separator.dx,
-        y1 = content_y + self.content.header.separator.dy,
-        x2 = content_x + self.content.header.separator.dx,
-        y2 = content_y + self.content.header.separator.dy + self.content.header.separator.h,
+        x1 = content_x + gui.header.separator.dx,
+        y1 = content_y + gui.header.separator.dy,
+        x2 = content_x + gui.header.separator.dx,
+        y2 = content_y + gui.header.separator.dy + gui.header.separator.h,
       }))
       assert(sdl_renderer:setDrawColor(prev_color))
-
     end
 
     _.each(self.drawing.objects, function(k, v) v:bind_ctx(details_ctx) end)
@@ -320,7 +323,6 @@ function BattleDetailsWindow:bind_ctx(context)
     self.content.mouse_click = mouse_click
     context.events_source.add_handler('mouse_move', mouse_move)
     self.content.mouse_move = mouse_move
-
 
     HorizontalPanel.bind_ctx(self, details_ctx)
   end
